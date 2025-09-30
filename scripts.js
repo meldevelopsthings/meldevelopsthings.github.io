36b20d2491b95e660e609eaa1fd55c1fc458728b
melody <melodygannon7@gmail.com>
2025-09-30 16:13:06 +0100

    j

--- scripts.js
+++ scripts.js
@@ -3,15 +3,17 @@ function menuClick() {
 
     if (click.style.display === "flex") {
         click.style.display = "none";
+        document.getElementById("menuButton").setAttribute("d", "M18 6L6 18M6 6L18 18");
     } else {
         click.style.display = "flex";
+        document.getElementById("menuButton").setAttribute("d", "M4 16H28M4 8H28M4 24H28");
     }
 }
 
-function menuToggle() {
-    if (document.getElementById("menuToggle").src == "./assets/menu.svg") {
-        document.getElementById("menuToggle").src = "./assets/exit.svg";
+/*document.getElementById("menuToggle").addEventListener("click", function() {
+    if (document.getElementById("mobileLinks").style.display === "flex") {
+        document.getElementById("menuButton").setAttribute("d", "M18 6L6 18M6 6L18 18");
     } else {
-        document.getElementById("menuToggle").src = "./assets/menu.svg";
+        document.getElementById("menuButton").setAttribute("d", "M4 16H28M4 8H28M4 24H28");
     }
-}
+});*/
